335b6af11775c7e86a6d37d142cec32680235752
Jatin Thacker <75543292+jatin-thacker@users.noreply.github.com>
2025-06-06 15:55:37 -0400

    Update analyze_id.py

--- analyze_id.py
+++ analyze_id.py
@@ -15,10 +15,11 @@ client = DocumentAnalysisClient(
 
 def extract_id_data(sas_url: str):
     try:
-        endpoint = os.getenv("AZURE_FORM_RECOGNIZER_ENDPOINT")
-        key = os.getenv("AZURE_FORM_RECOGNIZER_KEY")
+        #endpoint = os.getenv("AZURE_FORM_RECOGNIZER_ENDPOINT")
+        #key = os.getenv("AZURE_FORM_RECOGNIZER_KEY")
         
-        poller = client.begin_recognize_id_documents_from_url(sas_url)
+        #poller = client.begin_recognize_id_documents_from_url(sas_url)
+        poller = client.begin_analyze_document("prebuilt-idDocument", sas_url)
         result = poller.result()
 
         if not result:
