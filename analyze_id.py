c4465bc70b5a96b75e59c50aa56312e778144add
Jatin Thacker <75543292+jatin-thacker@users.noreply.github.com>
2025-06-06 17:54:29 -0400

    test

--- analyze_id.py
+++ analyze_id.py
@@ -17,9 +17,11 @@ def extract_id_data(sas_url: str):
     try:
         #endpoint = os.getenv("AZURE_FORM_RECOGNIZER_ENDPOINT")
         #key = os.getenv("AZURE_FORM_RECOGNIZER_KEY")
-        
+        image_url = r"https://smartidstorage123.blob.core.windows.net/ids/test1.jpg?sp=r&st=2025-06-05T20:02:28Z&se=2025-06-19T04:02:28Z&sv=2024-11-04&sr=c&sig=5e%2F%2BoCFrwEOBY0UlhNzvwABtc63AWAfHodI%2FAGYzHmY%3D"
+        image_data = requests.get(image_url).content
         #poller = client.begin_recognize_id_documents_from_url(sas_url)
-        poller = client.begin_analyze_document("prebuilt-idDocument", sas_url)
+        #poller = client.begin_analyze_document("prebuilt-idDocument", sas_url)
+        poller = client.begin_analyze_document("prebuilt-idDocument", document=image_data)
         result = poller.result()
 
         if not result:
