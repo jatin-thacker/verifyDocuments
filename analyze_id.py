616d2d7631d092ae2f34f1a4aa77bc90cf90dfae
Jatin Thacker <75543292+jatin-thacker@users.noreply.github.com>
2025-06-06 15:04:55 -0400

    Update analyze_id.py

--- analyze_id.py
+++ analyze_id.py
@@ -4,9 +4,16 @@ from azure.core.credentials import AzureKeyCredential
 from dotenv import load_dotenv
 
 load_dotenv()
+client = DocumentAnalysisClient(
+            endpoint=endpoint,
+            credential=AzureKeyCredential(key)
+        )
 
 def extract_id_data(sas_url: str):
     try:
+        endpoint = os.getenv("AZURE_FORM_RECOGNIZER_ENDPOINT")
+        key = os.getenv("AZURE_FORM_RECOGNIZER_KEY")
+        
         poller = client.begin_recognize_id_documents_from_url(sas_url)
         result = poller.result()
 
@@ -15,7 +22,7 @@ def extract_id_data(sas_url: str):
 
         # Just return the raw fields to see what we get
         fields = result[0].fields if result else {}
-        st.write("Raw fields:", fields)
+        # st.write("Raw fields:", fields)
         fields = result.documents[0].fields
         extracted = {
         "FirstName": fields.get("FirstName").value if fields.get("FirstName") else None,
